00324b8a6b6371d0082e6d662baeb19b003e22a5
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2024-07-07 19:00:31 +0300

    testing

---
+++ script1.sh
@@ -0,0 +1,14 @@
+echo "Today is " `date`
+echo ""
+#echo -e "\nenter the path to directory"
+#read the_path
+
+#echo -e "\n you path has the following files and folders: "
+#ls $the_path
+
+#git log
+read the_path
+git log --oneline
+echo $the_path
+echo $the_path
+echo $the_path

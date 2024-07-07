257a7db6b2cb916599f9999b95eec40c4bfae0d5
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2024-07-07 21:21:26 +0300

    Update submodules to latest commits

--- script1.sh
+++ script1.sh
@@ -4,7 +4,9 @@ git fetch
 # Loop through each submodule and check for updates
 git submodule foreach 'git fetch && git status && git pull origin $(git rev-parse --abbrev-ref HEAD)'
 
+
+echo "\n\n\n updating changes to the main repo"
 # Add and commit submodule updates in the main project
 git add .
-git commit -m "\n\n\n Update submodules to latest commits\n\n"
+git commit -m "Update submodules to latest commits"
 git push
